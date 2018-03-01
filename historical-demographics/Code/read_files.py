b8e8fa7fb82ee9480e4bef6006ed84c8449b71f5
Kira Schuman <kira.schuman@gmail.com>
2018-02-28 21:06:54 -0500

    Update

---
+++ historical-demographics/Code/read_files.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+bus_lic = pd.read_csv('../Data/business_licenses.csv')
+nta_shapes = pd.read_csv('../Data/nta_shapes.csv')
+nta_pops = pd.read_csv('../Data/New_York_City_Population_By_Neighborhood_Tabulation_Areas.csv')
+
